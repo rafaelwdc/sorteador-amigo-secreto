3b3683dd586bf1053feebe7e126d02be82edaa4a
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-06 01:45:08 -0300

    Fazendo o teste passar // 1. Bem começado, metade feito! => React: testando os seus componentes

--- src/componentes/Formulario.test.tsx
+++ src/componentes/Formulario.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
+import Formulario from "./Formulario";
 
 // Jest
 
@@ -7,20 +8,13 @@ test('Quando o input está vazio, novos participantes não podem ser adicionados
 
     render(<Formulario />)
 
-    // Encontrar no DOM o input
-
+    // -- Encontrar no DOM o input
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
-
-    // Encontrar o Botão
-
+    // -- Encontrar o Botão
     const botao = screen.getByRole('button')
-
-    // Garantir que o input esteja no Documento
-
+    // -- Garantir que o input esteja no Documento
     expect(input).toBeInTheDocument()
-
-    // Garantir que o botão esteja desabilitado
-
+    // -- Garantir que o botão esteja desabilitado
     expect(botao).toBeDisabled()
 
 })
