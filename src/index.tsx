8747a185b240d8e0604d3b6f1e2a2882b4291336
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-06 02:10:16 -0300

    TDD e a pirâmide de testes && Desafio: Implementando o cabeçalho // 1. Bem começado, metade feito! => React: testando os seus componentes

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Cabecalho from './Cabecalho';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Cabecalho />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
