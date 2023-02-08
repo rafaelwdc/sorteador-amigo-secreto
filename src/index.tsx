8bf6792a377ae19d03d8e07eb39b275bcb5732d9
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-07 21:52:13 -0300

    Desafio: evoluindo a estilização // 2. Testantando comportamentos => React: testando os seus componentes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Cabecalho from './Cabecalho';
+import Cabecalho from './componentes/Cabecalho';
 
 ReactDOM.render(
   <React.StrictMode>
