0bf847a72916cce55a5dfecd9c2c3e602760ae63
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-09 20:10:48 -0300

    Um hook para a todos sortear // 4. Sorteando amigos secretos => React: testando os seus componentes

--- src/state/atom.ts
+++ src/state/atom.ts
@@ -9,3 +9,8 @@ export const erroState = atom<string>({
     key: 'erroState',
     default: ''
 })
+
+export const resultadoDoAmigoSecreto = atom<Map<string, string>>({
+    key: 'erroState',
+    default: new Map()
+})
