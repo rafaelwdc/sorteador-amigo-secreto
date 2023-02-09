96567b27ba86bb510c69766f71db4df555b90053
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-02-09 02:01:06 -0300

    Fotografando componentes // 3. Finalizando a configuração => React: testando os seus componentes

---
+++ src/paginas/Configuracao.test.tsx
@@ -0,0 +1,19 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { RecoilRoot } from "recoil"
+import Configuracao from "./Configuracao";
+
+const mockNavegacao = jest.fn()
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavegacao
+    }
+});
+
+describe('A página de configuração,', () => {
+    test('Deve ser renderizada corretamente', () => { 
+        const { container } = render(<RecoilRoot><Configuracao /></RecoilRoot>)
+        expect(container).toMatchSnapshot()
+     })
+})
---
+++ src/paginas/Configuracao.tsx
@@ -0,0 +1,14 @@
+import Formulario from "../componentes/Formulario"
+import ListaParticipantes from "../componentes/ListaParticipantes"
+import Rodape from "../componentes/Rodape"
+
+const Configuracao = () => {
+    return (
+        <>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    )
+}
+export default Configuracao
